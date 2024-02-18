f830ddc68711d2922fa6bfc5815e3d8e56de95da
Zahid Afridi <119036431+zahid-afridi@users.noreply.github.com>
2024-02-18 14:08:57 +0500

    git user

--- src/Controllers/authControllers.js
+++ src/Controllers/authControllers.js
@@ -19,7 +19,13 @@ const Register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-res.send('hello form server')
-};
+    try {
+      const users = await User.find();
+      res.status(200).json({ success: true, users });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: "Internal server error" });
+    }
+  };
 
 export { login, Register };
