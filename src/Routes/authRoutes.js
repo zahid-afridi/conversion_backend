7d5410ae1c3642f4a40d82f7a28d73d0fa1693d3
Zahid Afridi <119036431+zahid-afridi@users.noreply.github.com>
2024-02-18 13:48:41 +0500

    add post to login

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -2,6 +2,6 @@ import express from 'express'
 import { Register, login } from '../Controllers/authControllers.js'
 const authrouter=express.Router()
 authrouter.post('/register',Register)
-authrouter.get('/login',login)
+authrouter.post('/login',login)
 
 export default authrouter
