534e969c58cdc0e4e17f7fc2a30754ea767411ea
Zahid Afridi <119036431+zahid-afridi@users.noreply.github.com>
2024-02-18 11:59:20 +0500

    add everything back

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,11 +1,7 @@
 import express from 'express'
 import { Register, login } from '../Controllers/authControllers.js'
 const authrouter=express.Router()
-// authrouter.post('/register',Register)
-// authrouter.post('/login',login)
-
-authrouter.get('/',(req,res)=>{
-    res.send("working")
-})
+authrouter.post('/register',Register)
+authrouter.post('/login',login)
 
 export default authrouter
