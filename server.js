24ce9ae5b23c83f998b429178c02afe1221a09b0
Zahid Afridi <119036431+zahid-afridi@users.noreply.github.com>
2024-02-18 07:36:43 +0500

    add home route

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ dotenv.config()
 const port=process.env.PORT
 const app=express()
 app.use(express.json())
-app.use('/api/auth',authrouter)
+// app.use('/api/auth',authrouter)
+app.use('/',(req,res)=>{
+    res.send('hy')
+})
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
