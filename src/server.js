b909c3146a0488e5457fcf79618fe21b4c38cd6d
Zahid Afridi <119036431+zahid-afridi@users.noreply.github.com>
2024-02-18 12:58:38 +0500

    add little code to serve.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authrouter from './Routes/authRoutes.js'
+import dbcon from './db/db.js'
 dotenv.config()
 const port=process.env.PORT
 const app=express()
 app.use(express.json())
 dbcon()
 app.use('/api/auth',authrouter)
-import dbcon from './db/db.js'
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
+    next()
   });
   
 
