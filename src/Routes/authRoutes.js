1b78c115a211c599f8555f7b7afd213f7ac3834d
Zahid Afridi <119036431+zahid-afridi@users.noreply.github.com>
2024-02-18 11:53:03 +0500

    add get for check

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import { Register, login } from '../Controllers/authControllers.js'
 const authrouter=express.Router()
-authrouter.post('/register',Register)
-authrouter.post('/login',login)
+// authrouter.post('/register',Register)
+// authrouter.post('/login',login)
+
+authrouter.get('/',(req,res)=>{
+    res.send("working")
+})
 
 export default authrouter
