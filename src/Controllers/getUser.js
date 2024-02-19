86113dea43a37401715b8d5bb4da5242b8cfc4e6
Zahid Afridi <119036431+zahid-afridi@users.noreply.github.com>
2024-02-19 23:24:30 +0500

    add Single User Api

--- src/Controllers/getUser.js
+++ src/Controllers/getUser.js
@@ -2,6 +2,7 @@ import User from '../Models/authModels.js'
 
 const GetUser=async(req,res)=>{
     try {
+      
       const users = await User.find();
       res.status(200).json({ success: true, users });
     } catch (error) {
@@ -9,5 +10,18 @@ const GetUser=async(req,res)=>{
       res.status(500).json({ success: false, message: "Internal server error" });
     }
   }
+  const SingleUser = async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const existingUser = await User.findById(userId);
+      if (!existingUser) {
+        return res.status(404).json({ success: false, message: "User not found" });
+      }
+      res.status(200).json({ success: true, user: existingUser });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: "Internal server error" });
+    }
+  };
 
-  export {GetUser}
+  export {GetUser,SingleUser}
--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { Register, login } from '../Controllers/authControllers.js'
-import { GetUser } from '../Controllers/getUser.js'
+import { GetUser, SingleUser } from '../Controllers/getUser.js'
 const authrouter=express.Router()
 authrouter.post('/register',Register)
 authrouter.post('/login',login)
 authrouter.get('/get-user',GetUser)
+authrouter.get('/User/:id',SingleUser)
 
 export default authrouter
