4a1acb9dce0ce882559aff280f5245e7e8470e08
Zahid Afridi <119036431+zahid-afridi@users.noreply.github.com>
2024-02-28 17:00:54 +0500

    add 30mints

--- src/Models/Messages.js
+++ src/Models/Messages.js
@@ -15,6 +15,6 @@ const MessageSchema = new mongoose.Schema({
 });
 
 // Create TTL index on createdAt field
-// MessageSchema.index({ createdAt: 1 }, { expireAfterSeconds: 15 });
+MessageSchema.index({ createdAt: 1 }, { expireAfterSeconds: 1800 }); // Expires after 30 minutes (1800 seconds)
 
 export default mongoose.model('Message', MessageSchema);
